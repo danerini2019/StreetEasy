8fe70bdf288076f72a4d609d7aa32efbae836862
Dane Rini <danerini@gmail.com>
2020-03-22 17:38:01 -0400

    Initial commit

--- NewYorkRealEstate.py
+++ NewYorkRealEstate.py
@@ -1,5 +1,14 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import scipy as stats
 
-print('This is my first real project')
+brooklyn_one_bed = pd.read_csv('brooklyn-one-bed.csv')
+brooklyn_price = brooklyn_one_bed['rent']
+
+manhattan_one_bed = pd.read_csv('manhattan-one-bed.csv')
+manhattan_price = manhattan_one_bed['rent']
+
+queens_one_bed = pd.read_csv('queens-one-bed.csv')
+queens_price = queens_one_bed['rent']
+
